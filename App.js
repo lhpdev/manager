bca977a80b08e6af7fd634cbe2d33ae45d40a3a3
Lucas <lucass.hauptmann@gmail.com>
2019-04-29 18:34:07 -0300

    Start Manager project: * Create LoginForm component * Setup firebase db and connect it to the project * Setup reducer and actions to handle user sign in and sign up (ongoing)

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
+
+export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyCdRhkQKbo7yo9apDhO9JVvZhuVl-wKapk",
+      authDomain: "manager-45adc.firebaseapp.com",
+      databaseURL: "https://manager-45adc.firebaseio.com",
+      projectId: "manager-45adc",
+      storageBucket: "manager-45adc.appspot.com",
+      messagingSenderId: "707157887924"
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+
+    );
+  }
+}
